d76ee9bf98b4e2a11247f15bc860d591c159a9a5
Shwetank <sinhashwetank74@gmail.com>
2023-07-10 15:17:11 +0530

    second commit

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const cities = require('./cities');
+const cities = require('country-state-city').City.getCitiesOfCountry("IN");
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
@@ -16,20 +16,20 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 300; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
+      const randomInt = Math.floor(Math.random() * cities.length);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             author: '64a5843a07af8684f5bf971f',
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            location: `${cities[randomInt].name}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
-            geometry:{ 
-              type: 'Point', 
+            geometry: {
+              type: "Point",
               coordinates: [
-                 cities[random1000].longitude,
-                 cities[random1000].latitude,
-             ] 
+                  cities[randomInt].longitude,
+                  cities[randomInt].latitude,
+              ]
             },
             images:[
               {
